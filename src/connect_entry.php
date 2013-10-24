8cb2b9b3b076fb47261e0e17b3da2beb6ce29012
Florent Chuffart <florent.chuffart@ens-lyon.fr>
2013-10-24 11:41:02 +0200

    Formnat connect_entry.php file

--- src/connect_entry.php
+++ src/connect_entry.php
@@ -1,13 +1,14 @@
 <?php
-define ('NOM', "root");
-define ('PASSE', "root");
-define ('SERVEUR', "localhost");
-define ('BASE', "gylab_stocks");
+define ('SERVEUR', "localhost");  // hostname for mysql server
+define ('BASE', "labstocks_db"); // name of the targeted db
+define ('LABNAME', "Demo");     // Name of the lab, appear in web page header
+define ('PASSE', "root");      // passwrd for db access
+define ('NOM', "root");       // username for db access
+// CONSTANTS, DO NOT CHANGE  //
 define ('PLASMAPPER_HOME', "/var/lib/tomcat6/webapps/PlasMapper/");
 define ('PLASMAPPER_SERVER', "http://" . $_SERVER["HTTP_HOST"] . ":8080/PlasMapper/");
 define ('LABSTOCK_SERVER', "http://" . $_SERVER["HTTP_HOST"] . "/labstocks/");
 define ('WWWBLAST_SERVER', "http://" . $_SERVER["HTTP_HOST"] . "/blast/");
 define ('BLAST_HOME', "/var/www/blast/db/");
 define ('FORMATDB_CMD', "formatdb");
-define ('LABNAME', "GY");
 ?>
