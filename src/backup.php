ed74d1012a9c648a0e48bd4e8d0f1347430e64ef
Florent Chuffart <florent.chuffart@ens-lyon.fr>
2014-04-11 10:42:02 +0200

    Fixing bug in backup.php

--- src/backup.php
+++ src/backup.php
@@ -36,7 +36,7 @@ $attachment_location = $absolute_backup_dir . ".tgz";
 if (file_exists($attachment_location)) {
   header($_SERVER["SERVER_PROTOCOL"] . " 200 OK");
   header("Cache-Control: public"); // needed for i.e.
-  header("Content-Type: application/gzip");
+  //header("Content-Type: application/gzip");
   header("Content-Transfer-Encoding: Binary");
   header("Content-Length:".filesize($attachment_location));
   header("Content-Disposition: attachment; filename=" . $backup_filename);
